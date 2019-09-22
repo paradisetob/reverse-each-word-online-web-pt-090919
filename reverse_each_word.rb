6f2be3046a3e5c39a771a6d90c83736e566e8472
tracey brimage <onelovenilynn@gmail.com>
2019-09-22 00:43:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word (sentence1)
+  reverse_each_word=[]
+  reverse_each_word.each do |sentence1|
+  "Hello there, and how are you?"
+ end
+sentence1.reverse
+ end
+end
+
+def reverse_each_word (sentence1)
+  reverse_each_word=[]
+  reverse_each_word.collect do |sentence1|
+     "Hello there, and how are you?"
+ end
+ sentence1.reverse
+ end
+end
