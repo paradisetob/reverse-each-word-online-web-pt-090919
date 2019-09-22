bdc991ac98335e78554228a8612613d5e30f33ec
tracey brimage <onelovenilynn@gmail.com>
2019-09-22 00:22:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (sentence1)
   reverse_each_word.each do |sentence1|
   "Hello there, and how are you?"
  end
-sentence1
+sentence1.reverse
 
 end
 
@@ -12,5 +12,5 @@ def reverse_each_word (sentence1)
   reverse_each_word.collect do |sentence1|
 sentence1= "Hello there, and how are you?"
  end
- sentence1
+ sentence1.reverse
 end
